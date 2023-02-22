c0c18f8161cec37d6d687b7d243c9e97ef263933
ohadk <ohadkachlon@gmail.com>
2023-02-22 13:09:36 +0200

    add react icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,11 @@ export function Header(){
     return(
 <header className={styles.header}>
     <h1>To-do list App</h1>
+    <form>
+        <input placeholder="add a new task" type="text"/>
+        <button>Create</button>
+        
+    </form>
 </header>
  )
  
